c731837792d50377db5a2a2a8904770a790407fd
nam <phung9nam@gmail.com>
2023-02-13 12:40:53 +0700

    init

---
+++ main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+
+class MyClass
+{
+
+public:
+    MyClass* func()
+    {
+
+        cout <<"\nfunc";
+        return this;
+    }
+    MyClass* func1()
+    {
+
+        cout <<"\nfunc1";
+         return this;
+    }
+    MyClass* func2()
+    {
+        cout <<"\nfunc2";
+        return this;
+    }
+
+};
+
+
+int main()
+{
+    MyClass my;
+    my.func()->func1()->func2()->func()->func1();
+    cout << "Hello World!" << endl;
+    return 0;
+}
